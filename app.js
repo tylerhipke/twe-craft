ff295d29871ad352c4e6889a71bd9cb5a0904805
Tyler Hipke <tylerhipke@gmail.com>
2021-03-24 16:50:18 -0700

    rename styles and add zip ons

--- app.js
+++ app.js
@@ -2,3 +2,22 @@
 // (c) nicole frank: the wardrobe evolution
 //
 
+$('#halfsleeveblue').click(function() {
+    $('#mainimg').attr("src","./zipon/blue.jpg");
+})
+
+$('#halfsleevepink').click(function() {
+    $('#mainimg').attr("src","./zipon/pink.jpg");
+})
+
+$('#halfsleevepython').click(function() {
+    $('#mainimg').attr("src","./zipon/python.jpg");
+})
+
+$("#collargold").click(function(){
+    $("#collar").toggle();
+})
+
+$("#collarblack").click(function(){
+    $("#collar").toggle();
+})
